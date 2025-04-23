3067e2f4eae3c9084069fb8b767b05f3b3d23233
UHa11 <extramin93@gmail.com>
2025-04-23 10:02:28 +0900

    no message

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' // 콘솔 두번뜬다.
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
