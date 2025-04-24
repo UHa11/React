52481c37e4bd01d981909c6cec4f975cb4e6d230
UHa11 <extramin93@gmail.com>
2025-04-24 15:32:58 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import React from 'react'
 import './App.css'
+import styled from 'styled-components'
+import TodoList from './components/TodoList'
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  background: #f3f2f2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
 
 function App() {
 
   return (
-    <>
-    </>
+    <AppContainer>
+      <TodoList />
+    </AppContainer>
   )
 }
 
