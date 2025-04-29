4dd44e691927316f0ea4e3f34eb2e17e4ebea530
UHa11 <extramin93@gmail.com>
2025-04-29 11:09:07 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import styled from 'styled-components'
 import './App.css'
-
+import CounterDisplay from './Components/CounterDisplay'
+const AppContainer = styled.div`
+  
+`
+const Section = styled.section`
+  width: 100%;
+`
 function App() {
 
   return (
-    <>
-    </>
+    <AppContainer>
+      <Section>
+        <h2>zustand 전역 상태 관리</h2>
+        <CounterDisplay />
+      </Section>
+    </AppContainer>
   )
 }
 
