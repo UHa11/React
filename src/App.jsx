b0a64088688d94aba40705c64784c15e295a4c44
UHa11 <extramin93@gmail.com>
2025-05-01 12:39:29 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/" style={{ marginRight: 12 }}>
+            Home
+          </Link>
+          <Link to="/login" style={{ marginRight: 12 }}>
+            로그인
+          </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomeForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
