8569cdf010c9932ff065e8edf31e75e12641e4f0
UHa11 <extramin93@gmail.com>
2025-05-01 21:12:45 +0900

    no message

---
+++ src/useContext/useContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const useContext = createContext();
+
+export function UserProvider({ children }) {
+  const [userList, setUserList] = useState([]);
+
+  return <UserContext.Provider value={{ userList, setUserList }}>{children}</UserContext.Provider>;
+}
+
+export function useUser() {
+  return useContext(useContext);
+}
