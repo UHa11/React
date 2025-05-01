e08da2a3fd7e50d406d72c3792b544b65d8d93a8
UHa11 <extramin93@gmail.com>
2025-05-01 21:08:58 +0900

    no message

--- src/components/SignUpView.jsx
+++ src/components/SignUpView.jsx
@@ -1,20 +1,84 @@
+import axios from 'axios';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  id: yup.string().required('아이디를 입력하세요'),
+  name: yup.string().required('이름을 입력하세요'),
+  age: yup.number().min(13),
+  phone: yup.number().min(10).max(11),
+  // email: yup.string().email('유효한 이메일 형식이 아닙니다.').required('이메일을 입력하세요'),
+  pwd: yup
+    .string()
+    .required('비밀번호를 입력해주세요')
+    .matches(/^(?=.*[a-zA-Z]).{5,}$/, '비밀번호는 영문자를 포함해 5자 이상이어야 합니다.'),
+  sPwd: yup.oneOf([yup.ref('pwd')], '비밀번호가 일치하지 않습니다'),
+});
 
 const SignUpView = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    const res = await axios.get('http://localhost:3001/users');
+    const users = res.data;
+
+    const foundUser = users.find((user) => user.id === data.id);
+    if (foundUser) {
+      setError('id', {
+        type: 'manual',
+        message: '이미 사용 중인 아이디입니다.',
+      });
+    } else {
+      const userProfile = {
+        id: data.id,
+        pwd: data.pwd,
+        sPwd: data.sPwd,
+        name: data.name,
+        age: data.age,
+        phone: data.phone,
+      };
+      navigate(`/${userProfile}`);
+    }
+  };
+
+  const onSearchId = () => {};
+
   return (
-    <div>
+    <form onSubmit={() => handleSubmit(onSubmit)}>
       <ul>
         <li>아이디</li>
-        <input type="text" />
+        <input type="text" {...register('id')} />
+        {errors.id && <p>{errors.id.message}</p>}
+        <button onClick={onSearchId}>중복확인</button>
         <li>비밀번호</li>
-        <input type="text" />
+        <input type="text" {...register('pwd')} />
+        {errors.pwd && <p>{errors.pwd.message}</p>}
         <li>비밀번호 확인</li>
-        <input type="text" />
+        <input type="text" {...register('sPwd')} />
+        {errors.sPwd && <p>{errors.sPwd.message}</p>}
+        <li>이름</li>
+        <input type="text" {...register('name')} />
+        {errors.name && <p>{errors.name.message}</p>}
+        <li>나이</li>
+        <input type="text" {...register('age')} />
+        {errors.age && <p>{errors.age.message}</p>}
         <li>폰 번호</li>
-        <input type="text" />
+        <input type="text" {...register('phone')} />
+        {errors.phone && <p>{errors.phone.message}</p>}
       </ul>
-      <button>회원가입</button>
-    </div>
+      <button type="submit">회원가입</button>
+    </form>
   );
 };
 
--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,34 +1,66 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+
+const schema = yup.object().shape({
+  id: yup.string().required('아이디를 입력하세요'),
+  pwd: yup.string().required('비밀번호를 입력해주세요'),
+});
 
 const LoginForm = () => {
-  const [users, setUsers] = useState([]);
   const navigate = useNavigate();
 
-  const getUsers = async () => {
-    const res = await axios.get('http://localhost:3001/users');
-    setUsers(res.data);
-  };
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const onLogin = () => {};
   const onSingUp = () => {
     navigate(`/signUp`);
   };
   const onSearchIdPwd = () => {
     navigate(`/searchIdPwd`);
   };
+
+  const onSubmit = async (data) => {
+    const res = await axios.get('http://localhost:3001/users');
+    const users = res.data;
+
+    const foundUser = users.find((user) => user.id === data.id);
+    if (foundUser) {
+      setError('id', {
+        type: 'manual',
+        message: '잘못된 아이디 입니다.',
+      });
+      setError('pwd', {
+        type: 'manual',
+        message: '잘못된 비밀번호 입니다.',
+      });
+    } else {
+      navigate(`/`);
+    }
+  };
+
   return (
-    <LoginContainer>
+    <LoginContainer onSubmit={() => handleSubmit(onSubmit)}>
       <h2>로그인</h2>
       <ul>
         <li>아이디</li>
-        <input type="text" />
+        <input type="text" {...register('id')} />
+        {errors.id && <p>{errors.id.message}</p>}
         <li>비밀번호</li>
-        <input type="text" />
+        <input type="text" {...register('pwd')} />
+        {errors.pwd && <p>{errors.pwd.message}</p>}
       </ul>
-      <button onClick={onLogin}>로그인</button>
+      <button type="submit">로그인</button>
       <button onClick={onSingUp}>회원 가입</button>
       <button onClick={onSearchIdPwd}>아이디/비번 찾기</button>
     </LoginContainer>
@@ -37,6 +69,6 @@ const LoginForm = () => {
 
 export default LoginForm;
 
-const LoginContainer = styled.div`
+const LoginContainer = styled.form`
   box-shadow: 0 0 8px hotpink;
 `;
